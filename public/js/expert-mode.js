269a95692c08dadb9c8761740dcb5c2d3b8d39e9
Will Crisp <will@elan2.com.au>
2023-06-19 20:35:01 +0800

    Expert Mode Class toggle added

---
+++ public/js/expert-mode.js
@@ -0,0 +1,28 @@
+//expert mode save to local storage
+var expertModeCheck = localStorage.getItem("expertModeCheck");
+if (expertModeCheck === null) {
+  expertModeCheck = false;
+} else {
+  expertModeCheck = JSON.parse(expertModeCheck);
+  $("#expertMode").prop("checked", expertModeCheck);
+}
+
+$(document).ready(function () {
+  if (expertModeCheck) {
+    $(".expertMode").removeClass("hidden");
+  } else {
+    $(".expertMode").addClass("hidden");
+  }
+});
+
+// Handle the checkbox change event
+$("#expertMode").change(function () {
+  expertModeCheck = $(this).is(":checked");
+  localStorage.setItem("expertModeCheck", JSON.stringify(expertModeCheck));
+
+  if (expertModeCheck) {
+    $(".expertMode").removeClass("hidden");
+  } else {
+    $(".expertMode").addClass("hidden");
+  }
+});
